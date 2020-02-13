28a2342a06c04088e03b48a44f48a991574cfeb1
Yue Yu <yuyue730@gmail.com>
2020-02-13 18:23:34 +0800

    Implement Queue data structure using Slice

---
+++ FundamentalGrammer/Queue/QueueEntry/entry.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	queue "GoLangIntro/FundamentalGrammer/Queue"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Go Language Queue Implementation")
+	q := queue.Queue{1}
+
+	q.Push(2)
+	q.Push(3)
+	fmt.Println("Pop 1 = ", q.Pop())
+	fmt.Println("Pop 2 = ", q.Pop())
+	fmt.Println("q isempty = ", q.IsEmpty())
+	fmt.Println("Pop 3 = ", q.Pop())
+	fmt.Println("q isempty = ", q.IsEmpty())
+}
