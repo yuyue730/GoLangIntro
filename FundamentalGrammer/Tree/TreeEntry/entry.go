eac57f8cd6ec655dcc79e88589488655b427b1fc
Yue Yu <yuyue730@gmail.com>
2020-02-13 18:09:23 +0800

    Extend TreeNode implementation by adding post order in entry

---
+++ FundamentalGrammer/Tree/TreeEntry/entry.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	tree "GoLangIntro/FundamentalGrammer/Tree"
+	"fmt"
+)
+
+type myTreeNode struct {
+	node *tree.Node
+}
+
+func (myNode *myTreeNode) postTraverse() {
+	if myNode == nil || myNode.node == nil {
+		return
+	}
+
+	left := myTreeNode{myNode.node.Left}
+	right := myTreeNode{myNode.node.Right}
+	left.postTraverse()
+	right.postTraverse()
+	myNode.node.Print()
+}
+
+func main() {
+	fmt.Println("Go language Objective Oriented Programming with Tree as an example")
+	var root tree.Node
+
+	root = tree.Node{Value: 3}
+	root.Left = &tree.Node{}
+	root.Right = &tree.Node{5, nil, nil}
+	root.Right.Left = new(tree.Node)
+	root.Left.Right = tree.CreateNode(2)
+	root.Right.Left.SetValue(4)
+	fmt.Printf("1. In-order traversal = ")
+	root.Traverse()
+	fmt.Println("\n")
+	fmt.Printf("2. Post-order traverse in entry main/package = ")
+	myRoot := myTreeNode{&root}
+	myRoot.postTraverse()
+	fmt.Println("\n")
+}
