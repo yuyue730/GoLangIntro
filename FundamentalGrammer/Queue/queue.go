a705631366732accffb2bbf1183822c6cec9a346
Yue Yu <yuyue730@gmail.com>
2020-02-16 21:34:48 +0800

    Documentation for Queue

--- FundamentalGrammer/Queue/queue.go
+++ FundamentalGrammer/Queue/queue.go
@@ -1,17 +1,21 @@
 package queue
 
+// An FIFO Queue
 type Queue []interface{}
 
+// Push an element into the queue
 func (q *Queue) Push(value interface{}) {
 	*q = append(*q, value)
 }
 
+// Pop element from queue
 func (q *Queue) Pop() interface{} {
 	head := (*q)[0]
 	*q = (*q)[1:]
 	return head
 }
 
+// Returns element is empty or not
 func (q *Queue) IsEmpty() bool {
 	return len(*q) == 0
 }
