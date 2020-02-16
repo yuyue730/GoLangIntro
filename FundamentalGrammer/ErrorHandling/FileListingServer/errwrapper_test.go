282f85f6275a7127dc99fe249e02b867121af67b
Yue Yu <yuyue730@gmail.com>
2020-02-16 21:16:39 +0800

    Unit Test for Error Wrapper

---
+++ FundamentalGrammer/ErrorHandling/FileListingServer/errwrapper_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+)
+
+func errPanic(writer http.ResponseWriter, request *http.Request) error {
+	panic(123)
+}
+
+type testingUserError string
+
+func (e testingUserError) Error() string {
+	return e.Message()
+}
+
+func (e testingUserError) Message() string {
+	return string(e)
+}
+
+func errUserError(writer http.ResponseWriter, request *http.Request) error {
+	return testingUserError("user error")
+}
+
+func errNotFound(writer http.ResponseWriter, request *http.Request) error {
+	return os.ErrNotExist
+}
+
+func errNoPermission(writer http.ResponseWriter, request *http.Request) error {
+	return os.ErrPermission
+}
+
+func errUnknown(writer http.ResponseWriter, request *http.Request) error {
+	return errors.New("Unknown error")
+}
+
+func noError(writer http.ResponseWriter, request *http.Request) error {
+	fmt.Fprintln(writer, "no error")
+	return nil
+}
+
+var tests = []struct {
+	h       appHandler
+	code    int
+	message string
+}{
+	{errPanic, 500, "Internal Server Error"},
+	{errUserError, 400, "user error"},
+	{errNotFound, 404, "Not Found"},
+	{errNoPermission, 403, "Forbidden"},
+	{errUnknown, 500, "Internal Server Error"},
+	{noError, 200, "no error"},
+}
+
+func TestErrorWrapper(t *testing.T) {
+	for _, tt := range tests {
+		f := errWrapper(tt.h)
+		response := httptest.NewRecorder()
+		request := httptest.NewRequest(http.MethodGet, "http://www.google.com", nil)
+		f(response, request)
+		verifyResponse(response.Result(), tt.code, tt.message, t)
+	}
+}
+
+func TestErrWrapperInServer(t *testing.T) {
+	for _, tt := range tests {
+		f := errWrapper(tt.h)
+		server := httptest.NewServer(http.HandlerFunc(f))
+		resp, _ := http.Get(server.URL)
+		verifyResponse(resp, tt.code, tt.message, t)
+	}
+}
+
+func verifyResponse(response *http.Response,
+	expectedCode int, expectedMessage string, t *testing.T) {
+	b, _ := ioutil.ReadAll(response.Body)
+	body := strings.Trim(string(b), "\n")
+	if response.StatusCode != expectedCode || body != expectedMessage {
+		t.Errorf("Expect (%d, %s), actual (%d, %s)",
+			expectedCode, expectedMessage, response.StatusCode, body)
+	}
+}
