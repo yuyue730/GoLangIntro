2f342aa6edd3179fe34903285c76b56505a8d9b2
Yue Yu <yuyue730@gmail.com>
2020-02-25 12:08:43 +0800

    Add implementation for processing otherCarList

--- SingleThreadCrawler/parser/carlist.go
+++ SingleThreadCrawler/parser/carlist.go
@@ -6,19 +6,37 @@ import (
 )
 
 var host = "http://newcar.xcar.com.cn"
-var carModelRe = `<a href="(/\d+/)" target="_blank" class="list_img">`
+var carModelRegex = `<a href="(/\d+/)" target="_blank" class="list_img">`
+var otherCarListRegex = `(//newcar.xcar.com.cn/car/[\d+-]+\d+)/`
 
+// This function starts to crawl for all car informations. It will look at two
+// parts of http://newcar.xcar.com.cn.
+// Part I: Car Model already show on this page, which will match `carModelRegex`
+// Part II: Additional Car List, which will match `otherCarListRegex`
 func ParseCarList(contents []byte) engine.ParseResult {
-	// Process already existed car models on main page
-	reModel := regexp.MustCompile(carModelRe)
-	matchesModel := reModel.FindAllSubmatch(contents, -1)
+	// Part I: Car Model already show on this page, which will match `carModelRegex`
+	regexpPtrModel := regexp.MustCompile(carModelRegex)
+	matchesModel := regexpPtrModel.FindAllSubmatch(contents, -1)
 
 	result := engine.ParseResult{}
 
 	for _, m := range matchesModel {
 		result.Requests = append(result.Requests, engine.Request{
-			Url:        host + string(m[1]), // This will be `http://newcar.xcar.com.cn/[id]`
+			Url: host + string(m[1]),
+			// This will be `http://newcar.xcar.com.cn/[id]`
 			ParserFunc: engine.NilParser,
+		})
+	}
+
+	// Part II: Additional Car List, which will match `otherCarListRegex`
+	regexpPtrOtherList := regexp.MustCompile(otherCarListRegex)
+	matchesOtherList := regexpPtrOtherList.FindAllSubmatch(contents, -1)
+
+	for _, m := range matchesOtherList {
+		result.Requests = append(result.Requests, engine.Request{
+			Url: "http:" + string(m[1]),
+			// This will be something like `http://newcar.xcar.com.cn/0-1-2-3-4-5`
+			ParserFunc: ParseCarList,
 		})
 	}
 
--- SingleThreadCrawler/parser/carlist_test.go
+++ SingleThreadCrawler/parser/carlist_test.go
@@ -15,13 +15,13 @@ func TestParseCarList(t *testing.T) {
 
 	results := ParseCarList(contents)
 
-	const resultModelSize = 20
-	if len(results.Requests) != resultModelSize {
-		t.Errorf("result size should have %d, but had %d", resultModelSize, 
+	const resultUrlSize = 89
+	if len(results.Requests) != resultUrlSize {
+		t.Errorf("result size should have %d, but had %d", resultUrlSize,
 			len(results.Requests))
 	}
 
-	expectedModelUrls := []string {
+	expectedModelUrls := []string{
 		"http://newcar.xcar.com.cn/3428/",
 		"http://newcar.xcar.com.cn/3796/",
 		"http://newcar.xcar.com.cn/4063/",
@@ -29,8 +29,23 @@ func TestParseCarList(t *testing.T) {
 
 	for i, url := range expectedModelUrls {
 		if results.Requests[i].Url != url {
-			t.Errorf("result index %d url should have %s, but had %s", i, 
+			t.Errorf("model result index %d url should have %s, but had %s", i,
 				expectedModelUrls[i], url)
+		}
+	}
+
+	const resultModelSize = 20
+	expectedOtherListUrls := []string{
+		"http://newcar.xcar.com.cn/car/0-0-0-0-0-0-0-0-0-0-0-1",
+		"http://newcar.xcar.com.cn/car/0-0-0-0-0-0-6-0-0-0-0-1",
+		"http://newcar.xcar.com.cn/car/0-0-0-0-0-0-8-0-0-0-0-1",
+	}
+
+	for i, url := range expectedOtherListUrls {
+		if results.Requests[resultModelSize+i].Url != url {
+			t.Errorf("other list result index %d url should have %s, but had %s",
+				i, expectedOtherListUrls[i],
+				results.Requests[resultModelSize+i].Url)
 		}
 	}
 }
